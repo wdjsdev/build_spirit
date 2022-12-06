380309be6d32dd737a68d49cc74dc182ed1b6c95
williams <31516287+wdjsdev@users.noreply.github.com>
2022-12-06 11:58:14 -0600

    Update choose_garments_to_process.js

--- components/choose_garments_to_process.js
+++ components/choose_garments_to_process.js
@@ -1,16 +1,18 @@
 function chooseGarmentsToProcess ( garments )
 {
 	var result = [];
-	var garNames = garments.map( function ( g ) { return g.garCode } );
+	// var garNames = garments.map( function ( g ) { return g.garCode } );
 
-	var chosenGarments = chooseFromListbox( garNames, "which garments" );
+	// var chosenGarments = chooseFromListbox( garNames, "which garments" );
+	var chosenGarments = chooseFromListbox( garments, "which garments" );
 
 
 	if ( chosenGarments && chosenGarments.length )
 	{
 		for ( var x = 0; x < chosenGarments.length; x++ )
 		{
-			result.push( garments[ garNames.indexOf( chosenGarments[ x ] ) ] );
+			// result.push( garments[ garNames.indexOf( chosenGarments[ x ] ) ] );
+			result.push( chosenGarments[ x ] );
 		}
 	}
 	return result;
