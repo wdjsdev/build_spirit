c092bd28611360fe309937c2a8469c714b5298c0
williams <31516287+wdjsdev@users.noreply.github.com>
2023-04-12 18:11:51 -0500

    Update find_prepress_file.js
    fix for finding the appropriate prepress when there are multiple prepresses with the same design number

--- components/find_prepress_file.js
+++ components/find_prepress_file.js
@@ -1,6 +1,6 @@
 function findPrepressFile ( garment )
 {
-	var result;
+	var resultFiles = [];
 	var files = Folder( prepressFolderPath ).getFiles().filter( function ( f ) { return f.name.match( /prepress.*\.ai[t]?$/i ) } );
 	var fName;
 	var code = garment.garCode.replace( /[-_]/g, "" );
@@ -10,12 +10,27 @@ function findPrepressFile ( garment )
 
 
 	findInFiles( files, searchTerm );
-	if ( !result )
+	if ( !resultFiles.length )
 	{
 		findInFiles( files, code.replace( /y/i, "" ).replace( /g/i, "W" ) );
 	}
 
+	if ( !resultFiles.length )
+	{
+		return;
+	}
 
+	if ( resultFiles.length > 1 )
+	{
+		var chosenFileName = chooseFromListbox( resultFiles.map( function ( f ) { return f.name } ), "Choose a prepress file for " + garment.label, [ 0, 0, 400, 200 ] );
+		// var result = files[ chosenFileName[ 0 ] ];
+		if ( !chosenFileName.length ) return;
+		var result = files.filter( function ( f ) { return f.name == chosenFileName[ 0 ] } )[ 0 ];
+	}
+	else
+	{
+		var result = resultFiles[ 0 ];
+	}
 
 	return result;
 
@@ -25,14 +40,10 @@ function findPrepressFile ( garment )
 	{
 		files.forEach( function ( f )
 		{
-			if ( result ) 
-			{
-				return;
-			}
 			if ( f.name.replace( /[-_]/g, "" ).match( searchTerm ) )
 			{
-				result = f;
-				log.l( "prepress file: " + result.name + " for " + garment.garCode + " found." )
+				resultFiles.push( f );
+				log.l( "prepress file: " + f.name + " for " + garment.garCode + " found." )
 			}
 		} );
 	}
